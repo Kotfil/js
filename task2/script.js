f6d5a8f3f9cc2c6103df0b258bc21b69e0040b20
Filip <39651273+Kotfil@users.noreply.github.com>
2019-05-22 16:56:16 +0300

    increment

---
+++ task2/script.js
@@ -0,0 +1,49 @@
+'use strict'
+
+
+//var number = 5;
+//var string = "Hello!";
+//var sym = Symbol();
+//var boolean = true;
+//null;
+//undefined;
+//var obj = {};   
+
+//console.log(4/0);
+//console.log ('string'*9);
+
+//let something;
+//console.log(something);
+
+//let persone = {
+
+    //name:   "John",
+   // age:    25,
+   // isMarried:  false
+//};
+
+//console.log(persone["name"]);
+
+//let arr = ['plum.png','orange.jpeg','apple.bmp'];
+
+//console.log(arr[0]);
+
+// alert("Hello World!");
+
+//let answer = confirm("Are you here?");
+
+//let answer = +prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof(answer));
+
+//console.log("arr" + " - object");
+//console.log(4 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.long(decr);
