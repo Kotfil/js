34a5b34a74d3ef810e7d054439a059f9f9b0ccd9
Filip <39651273+Kotfil@users.noreply.github.com>
2019-05-22 21:09:40 +0300

    table

---
+++ task3/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+    console.log(5*2);
+    console.log("2" === 2);
+
+    let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked && ! isClose);
