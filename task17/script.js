298897d7db75c560d574225bf5c0437381ce3742
Filip <39651273+Kotfil@users.noreply.github.com>
2019-06-29 11:30:44 +0300

    DOM

--- task17/script.js
+++ task17/script.js
@@ -1,14 +1,56 @@
-232312
-'string', "string", `string`;
-true / false 
+// 232312
+// 'string', "string", `string`;
+// true / false 
 
-//undefined
-let a;
-console.log(a);
+// //undefined
+// let a;
+// console.log(a);
 
-//массивы
-const obj = {
-		name:'Alex'
+// //массивы
+// const obj = {
+// 		name:'Alex'
+// }
+// 				0 1 
+// const arr = [1, 3, 5, 6,];
+
+// - to String
+
+// 1) 
+console.log(typeof(String(4)));
+// 2)
+console.log(typeof("" + false));
+console.log('https://vk.com/catalog/' + 5);
+
+// 1) - to Number
+
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(5 + + '5');
+
+// 3)
+
+console.log(typeof(parseInt('15px', 10)));
+
+let ans = +prompt("Hello?", "");
+
+
+// 0, ' ' , null, undefined, NaN
+
+let swicher = null;
+if (swicher){
+	console.log("Working..")
+
 }
-				0 1 
-const arr = [1, 3, 5, 6,];
+
+swicher = 1;
+
+if (swicher){
+	console.log("Working..")
+}
+
+// 2) 
+console.log(typeof(Boolean('5')));
+// 3)
+
+console.log(typeof(!!'5'));
