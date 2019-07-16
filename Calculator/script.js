0dfe12d7359dd5e3436d8c811c04be5c41c96335
Filip <39651273+Kotfil@users.noreply.github.com>
2019-07-16 16:46:35 +0300

    bool

--- Calculator/script.js
+++ Calculator/script.js
@@ -9,3 +9,26 @@ function plus (){
 	//innerHTML
 	document.getElementById('out').innerHTML = result;
 }
+
+function minus() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1 - num2;
+	//innerHTML
+	document.getElementById('out').innerHTML = result;
+}
+function multiply() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1 * num2;
+	//innerHTML
+	document.getElementById('out').innerHTML = result;
+}
