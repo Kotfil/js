f47c273c9e0bad7a1d8e4acd212e426a9a26d2af
Filip <39651273+Kotfil@users.noreply.github.com>
2019-08-15 20:24:14 +0300

    battery

--- task18/script.js
+++ task18/script.js
@@ -3,6 +3,7 @@ btn = document.getElementsByTagName('button');
 circle = document.getElementsByClassName('circle');
 heart = document.querySelectorAll('.heart');
 oneHeart = document.querySelector('.heart');
+wrapper = document.querySelector('.wrapper');
 
 
 box.style.backgroundColor = 'blue';
@@ -13,10 +14,30 @@ circle[1].style.backgroundColor = 'yellow';
 circle[2].style.backgroundColor = 'green';
 
 //for(let     i = 0; i<heart.length; i++){
-    heart[i].style.backgroundColor = 'blue'
-}
+  //  heart[i].style.backgroundColor = 'blue'
+//}
 
-heart.forEach(function(item, i, hearts){
+//heart.forEach(function(item, i, hearts){
 
-item.style.backgroundColor = 'blue';
-});
+//item.style.backgroundColor = 'blue';
+//});
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я');
+
+    div.classList.add('black');
+    
+   // document.body.appendChild(div);
+  // wrapper.appendChild(div);
+
+//div.innerHTML = '<h1>Hello World!</h1>';
+div.textContent = 'Hello World!';
+
+
+  document.body.insertBefore(div, circle[0]);
+  document.body.removeChild(circle[1]);
+  wrapper.removeChild(heart[1]);
+  
+  document.body.replaceChild(btn[1], circle[1] );
+console.log(div);
+
---
+++ task21/script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementsByTagName('button');
+
+btn[0].onclick = function(){
+    alert('Вы нажали первую кнопку!');
+};
+
+btn[0].onclick = function(){
+    alert('Вы нажали опять первую кнопку!');
+};
