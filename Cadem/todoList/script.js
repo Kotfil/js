e305dc69d3d35ac3f8416ff2711dfa25c2b6630f
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-08-30 01:50:31 +0300

    todoDone

--- Cadem/todoList/script.js
+++ Cadem/todoList/script.js
@@ -1,14 +1,19 @@
-const selecteAllButton = document.getElementById('test')
+
 const inputElement = document.getElementById('input')
 const ulElement = document.getElementById('list')
+const actionPanel1 = document.getElementById('actionPanel1')
+const actionPanel2const  = document.getElementById('actionPanel2')
 
 let todoList = []
- //upgradeView()
+//actionPanel2.style.display = 'none'
+
+ 
+ 
 
  inputElement.addEventListener('keydown', event => {
-    if (event.key === 'Enter' || event.keyCode === 13) {
+    if ((event.key === 'Enter' || event.keyCode === 13) && (inputElement.value)) {
         todoList.unshift({
-			content:inputElement.value,
+			content: inputElement.value,
 			done: false,
 			selected: false
 		})
@@ -18,38 +23,125 @@ let todoList = []
 	}
 })
 
+    function upgradeView(){
+        ulElement.innerHTML = ''
+        
+        
+            for(let index = 0; index < todoList.length; index++){
+                const todoItem = todoList[index]
 
-function upgradeView() { 
-
-	
       const liElement = document.createElement('li')
-      liElement.className = 'list-grpup-item'
+      liElement.className = 'list-group-item'
       ulElement.append(liElement)
 
       const divElement = document.createElement('div')
-      divElement.className = 'list-group form-check'
+      divElement.className = 'form-group form-check'
       liElement.append(divElement)
 
       const checkboxElement = document.createElement('input')
       divElement.append(checkboxElement)
       checkboxElement.type = 'checkbox'
       checkboxElement.className = 'form-check-input'
+      checkboxElement.id = 'todoItem' + index 
+      checkboxElement.checked = todoItem.selected
 
       const labelElement = document.createElement('label')
       divElement.append(labelElement)
       labelElement.className = 'form-check-label'
-      labelElement.setAttribute('for', 'exampeCheck1')
-      labelElement.innerText = 'Check me out'
+      if (todoItem.done){
+          labelElement.className += ' todoDone'
+      }
+      labelElement.setAttribute('for', 'todoItem' + index)
+      labelElement.innerText = todoItem.content
 
-      const buttonDoneElement = document.createElement('button')
+      if(!todoItem.done){
+        const buttonDoneElement = document.createElement('button')
       divElement.append(buttonDoneElement)
       buttonDoneElement.type = 'button'
       buttonDoneElement.className = 'btn btn-outline-primary'
       buttonDoneElement.innerText = 'Done'
+      buttonDoneElement.style = 'float: right'
 
-      const buttonRemoveElement = document.createElement('button')
-      divElement.append(buttonRemoveElement)
-      buttonRemoveElement.type = 'button'
-      buttonRemoveElement.className = 'btn btn-outline-danger'
-      buttonRemoveElement.innerText = 'Remove'
+      buttonDoneElement.addEventListener('click', () => {
+            todoItem.done = !todoItem.done
+            upgradeView()
+            })
+    }
+    
+    else {
+          const buttonRemoveElement = document.createElement('button')
+          divElement.append(buttonRemoveElement)
+          buttonRemoveElement.type = 'button'
+          buttonRemoveElement.className = 'btn btn-outline-danger'
+          buttonRemoveElement.innerText = 'Remove'
+          buttonRemoveElement.style = 'float: right '  
+          buttonRemoveElement.addEventListener('click', () => {
+            
+                todoList = todoList.filter(currentTodoItem => currentTodoItem !== todoItem)
+                upgradeView()
+            })
+        }
+
+          checkboxElement.addEventListener('change', () => {
+              todoItem.selected = checkboxElement.checked
+              upgradeView()
+          })
+        }
+
+        const someSelected = todoList.some(todoItem => todoItem.selected)
+        if (someSelected) {
+            actionPanel1.style.display = 'none'
+            actionPanel2.style.display = 'block'
+    } else {
+        actionPanel1.style.display = 'flex'
+            actionPanel2.style.display = 'none'
+    }
 }
+
+document.getElementById('doneAction').addEventListener('click', () => {
+    for (const todoItem of todoList){
+        if (todoItem.selected){
+            todoItem.done = true
+            todoItem.selected = false
+        }
+       
+
+    }
+    upgradeView()
+})
+
+document.getElementById('restoreAction').addEventListener('click', () => {
+    for (const todoItem of todoList){
+        if (todoItem.selected){
+            todoItem.done = false
+            todoItem.selected = false
+        }
+       
+
+    }
+    upgradeView()
+})
+
+document.getElementById('removeAction').addEventListener('click', () => {
+   todoList = todoList.filter(todoItem => !todoItem.selected)
+    // // for (const todoItem of todoList){
+    //     if (todoItem.selected){
+    //         todoItem.done = false
+    //         todoItem.selected = false
+    //     }
+       
+
+    // }
+    upgradeView()
+})
+
+document.getElementById('test').addEventListener('click', () => {
+    for (const todoItem of todoList){
+        todoItem.selected = true
+        }
+       
+
+    
+    upgradeView()
+})
+
