84f7508c31fc080abe2e31376fbd6e87b0fa901a
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-09-18 17:28:23 +0300

    study

---
+++ task/script.js
@@ -0,0 +1,103 @@
+ // let num = 25;
+
+// // function showFirstMessage(text) {
+// //     alert(text);
+// //     let num = 21;
+// //     console.log(num);
+// // }
+
+// // showFirstMessage("ПРИВЕЕЕЕЕТ");
+// // console.log(num);
+
+// function calc(a,b){
+//     return (a+b);
+// }
+// console.log(calc(2,2))
+// console.log(calc(8,23))
+
+// function retVar(){
+//     let num = 50;
+//     return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+
+
+// console.log(calc);
+// let calc = function(a,b){
+//     return (a+b);
+// }
+// let calc = (a,b) => a+b
+// console.log(calc);
+
+
+
+// if(num < 49){
+//     console.log('Неверно')
+// } else if (num > 100){
+//     console.log("Много")
+// } else {
+//     console.log('Верно')
+// };
+
+
+// (num == 50) ? console.log ('Верно!') : console.log ('Неверно');
+
+
+// switch (num) {
+//     case num < 49:
+//             console.log('Неверно');
+//             break;
+//             case num > 100:
+//                     console.log("Много");
+//                     break;
+//                     case num > 80:
+//                         console.log('Все еще много');
+//                         break;
+//                         case 50:
+//                                 console.log('Верно');
+//                                 break;
+//                                 default:
+//                                     console.log("Что то пошло не совсем так");
+//                                     break;
+
+// }
+
+// let num = 50; 
+// // while(num < 7500) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
+//     console.log(num);
+//     num++;  
+// }
+// while (num < 55);
+
+// for(let i = 1; i < 8; i++){
+//     if (i == 9)
+//     continue
+//     console.log(i)
+// }
+
+let str = "Vasiliski";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+//console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
+
+
+
+
+
+
+
+
