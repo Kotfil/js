7b41fa81c98fb3c8cad903c167a61a0e49ea273b
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-09-19 23:21:12 +0300

    123

--- task1/script.js
+++ task1/script.js
@@ -1,63 +1,21 @@
-'use strict';
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-a = 5;
-console.log(a);
-
-var number =5;
-var string = "Hello";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
-
-console.log (4/0);
-console.log ('string' *9);
-
-let something;
-console.log (something)
-
-let Leningradka ={
-    diser: "Malecha",
-    age: 23,
-    isMarried: false
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-console.log (Leningradka["diser"],["age"]);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-let Sosedi = ['sssr.png','dasha.jpg','grywa.bmp'];
-console.log(Sosedi[2]);
-
-
-//alert("Love Vikimiki");
-
-// let answer = confirm("Do you love?");
-
-// console.log (answer);
-
-//let answer = +prompt("Есть ли вам 20 молодой человек?", "Да");
-
-// console.log (typeof(answer));
-
-// console.log ("sosed" + " - object");
-// console.log (4 + +" - object");
-
-let incr = 10,
-    decr = 10;
-
-   
-    console.log(decr--),
-    console.log(incr++);
-
-    console.log (5%2);
-
-    console.log("2" == 2);
-
-
-    let 
-        isMom = false,
-        isBabs = false;
-    
-   console.log(!isMom || isBabs);
-      
+alert(appData.budget / 30);
