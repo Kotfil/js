055f405e515b1e27f4a47f34667b079b9c6a60a2
FilippK <filippkotenko@outlook.com>
2020-03-22 18:55:36 +0200

    JS numbers

---
+++ CodeMU/StartJS/script.js
@@ -0,0 +1,83 @@
+'use strict'
+// let a ,b ,c;
+// //
+// // a = 1
+// // b = 22
+// // c = 30
+// // alert(a);
+// // alert(b);
+// // alert(c);
+// let a;
+//
+// a = 1;
+// alert(a);
+//
+// a = 2;
+// alert (a);
+// let a;
+// // a = 10;
+// // alert(a);
+// // a = 20;
+// // alert(a);
+// let a = 1 + 2;
+// alert(a);
+// let b = 3 - 1;
+// alert(b);
+// let c = 3*5;
+// alert(c);
+// let d = 10/2;
+// alert(d);
+// let a = 1+2+3;
+// alert(a);
+// // alert(2+20);
+// alert(1 + 2 + 10);
+// let a=1;
+// let b=2;
+// alert(a+b);
+
+// let a=1;
+// let b=2;
+// let c=a+b;
+// alert(c);
+// let c = 10;
+// let d = 5;
+// let result = c + d;
+// alert(result);
+// let a  = 1;
+// let b = 22;
+// let c = 50;
+// alert(a+b+c);
+// let a = 10;
+// let b = 5;
+// let c = a-b;
+// let d = 7;
+// let result = c+d;
+// alert(result);
+//
+// let a = 2 * 2 + 3;
+// alert(a);
+// let a = 5 + 5 * 3;
+// alert(a);
+// let a = 5 + 5 * 3 + 3;
+// alert(a);
+// let a = 8 / 2 + 2;
+// alert(a);
+// let a = 2 / 2;
+// alert(a);
+
+// let a = 8 / 2 * 4;
+// alert(a);
+// let a = 2 * (2 + 3);
+// // alert(a);
+// let a = 2 * (2 + 4 * (3 + 1));
+// alert(a);
+// let a = (2 + 3) * (2 + 3);
+// alert(a);
+// let a = (2 + 3) * 2 + 3;
+// alert(a);
+// let a = 2 * (2 + 4 * (3 + 1));
+// // alert(a);
+// let a = 2 * 8 / 4;
+// alert(a);
+let a = (2 * 8) / 4;
+alert(a);
